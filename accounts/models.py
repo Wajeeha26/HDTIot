8479d972740dbafc4d69b3d651e8f7a90f6ba867
Wajeeha26 <wajeehamqazi269@gmail.com>
2025-03-05 15:32:25 +0500

    first commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    uid = models.CharField(max_length=255, unique=True)  # Firebase UID
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    role = models.CharField(max_length=20, choices=[('patient', 'Patient'), ('doctor', 'Doctor')])
+
+    def __str__(self):
+        return self.email
